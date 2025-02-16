89dbc8e8c384e7000fc56d37130c275d2859601b
Krishna Panjiyar <kp05008@hotmail.com>
2025-02-16 21:24:03 +0530

    edited with n

--- DSA-0/pattern5.cpp
+++ DSA-0/pattern5.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
-int main()
+int pattern5(int n)
 {
-    for (int i=0;i<5;i++){
-        for (int j=0;j<5-i;j++){
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n-i;j++){
             cout << "* ";
         }
         cout << endl;
     }
+    return 0;
+}
+
+int main(){
+pattern5(5);
+return 0;
 }
