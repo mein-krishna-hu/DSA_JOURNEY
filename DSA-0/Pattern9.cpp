a2ec113dde236a944cdfb27308af3b2df344e48d
Krishna Panjiyar <kp05008@gmail.com>
2025-03-02 00:12:49 +0530

    pushed

--- DSA-0/Pattern9.cpp
+++ DSA-0/Pattern9.cpp
@@ -8,7 +8,18 @@
 using namespace std;
 int pattern8(int n) {
     for (int i=0;i<n;i++){
-        //space
+          //space
+          for( int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j = 0;j< 2*i+1;j++){
+            cout<<"*";
+        }
+        //for space
+        for(int j = 0;j<n-i-1;j++){
+cout << " ";
+        } //space
         for( int j=0;j<i;j++){
             cout<<" ";
         }
@@ -30,9 +41,4 @@ int main(){
     pattern8(5);
     return 0;
 }
-//print the pattern below
-//*********
-// *******
-//  *****
-//   ***
-//    *
+
