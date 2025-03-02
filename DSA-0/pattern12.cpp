03e4c7129719a8b771670726f7e8a62130d4ea7a
Krishna Panjiyar <kp05008@hotmail.com>
2025-03-02 19:36:22 +0530

    push

---
+++ DSA-0/pattern12.cpp
@@ -0,0 +1,12 @@
+// https://static.takeuforward.org/wp/uploads/2022/08/P12.png
+
+#include <iostream>
+using namespace std;
+int pattern12(int n){
+
+}
+
+int main() {
+    pattern12(5)    ;
+    return 0;
+}
