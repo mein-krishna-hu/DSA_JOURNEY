221d7ce833016c5a70b6bd0dc12644b5d6d10ef5
Krishna Panjiyar <kp05008@gmail.com>
2025-03-02 15:07:26 +0530

    pattern9

--- DSA-0/Pattern9.cpp
+++ DSA-0/Pattern9.cpp
@@ -1,25 +1,30 @@
-// *********
-//  *******
-//   *****
-//    ***
-//     *
-//print this pattern
+// https://static.takeuforward.org/wp/uploads/2022/08/P9.png
+// print this pattern
 #include <iostream>
 using namespace std;
-int pattern8(int n) {
+int pattern9(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+        {
+            cout << "*";
+        }
+        // for space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
     for (int i=0;i<n;i++){
-          //space
-          for( int j=0;j<n-i-1;j++){
-            cout<<" ";
-        }
-        //star
-        for(int j = 0;j< 2*i+1;j++){
-            cout<<"*";
-        }
-        //for space
-        for(int j = 0;j<n-i-1;j++){
-cout << " ";
-        } //space
+        //space
         for( int j=0;j<i;j++){
             cout<<" ";
         }
@@ -33,12 +38,10 @@ cout << " ";
         }
         cout << endl;
     }
-
-    return 0;
 }
 
-int main(){
-    pattern8(5);
+int main()
+{
+    pattern9(5);
     return 0;
 }
-
